03bc47f1963bb00a37772a8311a0070ef3793ce3
Justusifeanyi <Justusifeanyie@gmail.com>
2022-09-15 21:58:46 -0700

    Project files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase character
+ * @c: a parameter
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - A function that checks for a digit 0 through 9
+ * @c: A parameter
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
